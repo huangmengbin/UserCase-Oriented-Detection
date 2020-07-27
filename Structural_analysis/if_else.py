31fee36029ef2883661332c902fb6ff21849f5c2
yuanyifeng <181250179@smail.nju.edu.cn>
2020-07-27 16:57:12 +0800

    if数据

--- Structural_analysis/if_else.py
+++ Structural_analysis/if_else.py
@@ -48,32 +48,9 @@ if 10086 == 67666:
 
 
 
-# root_node = ast.parse(code)
-# print(astunparse.dump(root_node))
-# for node in ast.walk(root_node):
-#     if isinstance(node,ast.If):
-#         print(node)
-#         for node_ in ast.walk(node):
-#             if isinstance(node_,ast.Compare):
-#                 print(type(node_))
-#                 for node__ in ast.walk(node_):
-#                     if isinstance(node__,ast.Num):
-#                         print(ast.dump(node__))
-#         break
-    # if isinstance(node, ast.If):
-    #     todo_yyf = '''
-    #     if的数量和用例数量相差多少，一两个是危险的情况
-    #     if里面body的内容是不是非常统一且简短，只有一两行
-    #     可以把某个if结点删除了，再对用例分别exec，看看是哪一个用例不能过
-    #     或者是把if里面的body弄成空，相当于注释掉if里面的内容，也ok的
-    #     再删除另一个if节点，重复直至0分
-    #     记得捕获异常
-    #     设计一个xx公式，最终算出一个[0,1]区间的xx度
-    #     '''
-    #     print(type(node), node, '   ==>  ', node.__dict__)
 if_=[]
 print_=[]
-expr_ast=ast.parse(code)
+#expr_ast=ast.parse(code)
 # print(astunparse.dump(expr_ast))
 
 
@@ -96,60 +73,43 @@ def findAllSimpleIF(rootNode):
 def hmbTest():
     rootNode = ast.parse(code)
     result = findAllSimpleIF(rootNode)
-
+    print(len(result))
     for node in result:
-        print('=' * 67)
-        print(ast.dump(node.test))
-        print()
-        print(node.body)
-        print()
-        for middleNode in node.body:
-            for smallNode in ast.walk(middleNode):
-                print(ast.dump(smallNode))
-
-def _if_():
-    lis_ = []
-    if_data=[]
-    for one in ast.walk(expr_ast):
-        if isinstance(one, ast.If):
-            for two in ast.walk(one):
-                #print(type(two))
-                if isinstance(two, ast.Call):
-                    if (lis_ != []):
-                        # print(lis_)
-                        if_data.append(lis_)
-                        lis_ = []
-                if isinstance(two, ast.Compare):
-                    for thr in ast.walk(two):
-                        if isinstance(thr, ast.Num) or isinstance(thr, ast.Str):
-                            s = (ast.dump(thr))
-                            # print(s)
-                            lis_.append(s[s.find('=') + 1:len(s) - 1])
-    num_if=astunparse.dump(expr_ast).count("If")
-    print("if:",end="")
-    if_=if_data[0:num_if]
+        if_.append(_if_(node))
+        print_.append(_print_(node))
     print(if_)
+    print(print_)
 
-def _print_():
+def _if_(expr_ast):
     lis_ = []
-    for one in ast.walk(expr_ast):
-        if isinstance(one, ast.If):
-            for two in ast.walk(one):
-                if isinstance(two, ast.Expr):
-                    for thr in ast.walk(two):
-                        if isinstance(thr, ast.Num) or isinstance(thr, ast.Str):
-                            s = (ast.dump(thr))
-                            # print(s)
-                            if(s[s.find('=') + 1:len(s) - 1]=="''"):
-                                continue
-                            lis_.append(s[s.find('=') + 1:len(s) - 1])
-            break
-    print_=lis_
-    print("print:",end="")
-    print(lis_)
+    test=expr_ast.test
+    for node in ast.walk(test):
+        if isinstance(node,ast.Compare):
+            for node_ in ast.walk(node):
+                if isinstance(node_, ast.Num) or isinstance(node_, ast.Str):
+                    s = (ast.dump(node_))
+                    lis_.append(s[s.find('=') + 1:len(s) - 1])
+    # print(lis_)
+    return lis_
+
+def _print_(expr_ast):
+    lis_ = []
+    body = expr_ast.body
+    for node in body:
+        # print(type(node))
+        if isinstance(node,ast.Assign) or isinstance(node,ast.Expr):
+            for node_ in ast.walk(node):
+                if isinstance(node_, ast.Num) or isinstance(node_, ast.Str):
+                    s = (ast.dump(node_))
+                    if (s[s.find('=') + 1:len(s) - 1] == "''"):
+                        continue
+                    lis_.append(s[s.find('=') + 1:len(s) - 1])
+
+
+
+    # print(lis_)
+    return lis_
 
 
 if __name__ == '__main__':
     hmbTest()
-    # _if_()
-    # _print_()
